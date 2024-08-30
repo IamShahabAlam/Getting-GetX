4921f700fd611c52ba3c7a7e4d7234e3e3228fac
Shahab Alam <iamShahabAlam@gmail.com>
2024-08-30 15:05:40 +0500

    New Arch Routes

---
+++ lib/routes/app_routes.dart
@@ -0,0 +1,12 @@
+class PageNames {
+  static const main = "/main";
+  static const screen1 = "/screen1";
+  static const screen2 = "/screen2";
+  static const screen3 = "/screen3";
+  static const screen4 = '/screen4/';
+  static const screen5 = "/screen5";
+  static const screen6 = "/screen6";
+  static const screen7 = "/screen7";
+  static const screen8 = "/screen8";
+  // static const = ;
+}
