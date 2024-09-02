6b1695c96e5fe1101a56dffbc519dbb9d5afe5d1
Shahab Alam <iamShahabAlam@gmail.com>
2024-09-02 23:01:37 +0500

    new Arch

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:getx_app/screen1.dart';
-import 'package:getx_app/screen2.dart';
-import 'package:getx_app/screen3.dart';
-import 'package:getx_app/screen4.dart';
-import 'package:getx_app/screen5.dart';
-import 'package:getx_app/screen6.dart';
-import 'package:getx_app/screen7.dart';
-import 'package:getx_app/screen8.dart';
-import 'package:getx_app/wrongRoute.dart';
-import 'services.dart';
+import 'package:getx_app/presentation/general/general_controller.dart';
+import 'package:getx_app/presentation/general/views/screen1.dart';
+import 'package:getx_app/routes/page_names.dart';
+import 'package:getx_app/routes/app_routes.dart';
+import 'package:getx_app/presentation/wrong_route/wrong_route.dart';
 
 void main() {
   AllBindings().dependencies();
@@ -22,34 +17,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       initialBinding: AllBindings(),
-      translations:
-          Messages(), // my class which holds messages in all languages (Internationalization)
+      translations: Messages(), // my class which holds messages in all languages (Internationalization)
       locale: Locale("en", "US"), // setting default language
       //locale: Get.deviceLocale,   // Gets device default language
-      fallbackLocale: Locale("en",
-          "US"), // when doesn't find right device lang , switches to here this
-      debugShowCheckedModeBanner: false,
-      initialRoute: "/screen1",
+      fallbackLocale: Locale("en", "US"), // when doesn't find right device lang , switches to here this
+      initialRoute: PageNames.screen1,
       unknownRoute: GetPage(name: "/wrong", page: () => WrongRoute()),
-      getPages: [
-        GetPage(name: "/main", page: () => MyApp()),
-        GetPage(name: "/screen1", page: () => Screen1()),
-        GetPage(name: "/screen2", page: () => Screen2()),
-        GetPage(name: "/screen3", page: () => Screen3()),
-        GetPage(name: '/screen4/:City', page: () => Screen4()), // Passing data with parameter
-        GetPage(name: "/screen5", page: () => Screen5()), // Simple State Manager
-        GetPage(name: "/screen6", page: () => Screen6()),
-        GetPage(name: "/screen7", page: () => Screen7()),
-        GetPage(name: "/screen8", page: () => Screen8()),
-        
-      ],
-
+      getPages: appRoutes.appRoutesList,
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
+      theme: ThemeData(primarySwatch: Colors.purple),
+      home: Scaffold(
+        body: Screen1()
       ),
-      home: Screen1(),
     );
   }
 }
--- lib/presentation/wrong_route/wrong_route.dart
+++ lib/presentation/wrong_route/wrong_route.dart
@@ -12,18 +12,18 @@ class WrongRoute extends StatelessWidget {
       body: SafeArea(
         child: Column(
           children: [
-            SizedBox(height: Get.context.height*0.08,),
+            SizedBox(height: Get.height*0.08,),
             Center(child:
                Image.network(
                  "https://user-images.githubusercontent.com/82330891/201894928-484d551c-2a27-48f5-9e22-1aeb6051a21e.gif",
                 //  "https://ibb.co/31tmG2y",
                 //  "https://i.ibb.co/VVXYrRW/wrong-Route.gif",
                    alignment : Alignment.center,
-                 height: Get.context.height*0.6,
-                 width: Get.context.width*0.8,
+                 height: Get.height*0.6,
+                 width: Get.width*0.8,
                )),
 /// COPYYYYYY MEDIAQUERY
-            SizedBox(height: Get.context.height*0.1),
+            SizedBox(height: Get.height*0.1),
 
             Center(
               child: HoverButton(
@@ -38,8 +38,8 @@ class WrongRoute extends StatelessWidget {
                 onpressed:(){ Get.back(); },
 
                 child: Container(
-                  height: Get.context.height*0.1,
-                  width: Get.context.width*0.3,
+                  height: Get.height*0.1,
+                  width: Get.width*0.3,
                   child: Row( mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: [
                       Icon(Icons.arrow_back_ios_new ),
